6c5863e0d0cb8718a26719d7508cd20b86d9503f
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-22 09:09:25 +0200

    Add files via upload

---
+++ JS_opdracht01/script.js
@@ -0,0 +1,14 @@
+let name = "Kasper";
+        
+let A = 4
+let B = "4"
+
+console.log("Hello Winc Academy!") // Hello Winc Academy!
+console.log(name) // Kasper
+console.log(A+A) // 8
+console.log(B+B) // 44, dus nee
+
+console.log(name + B)
+
+let age = 28
+typeof age
