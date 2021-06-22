16741a2591ef1d8e539f09f65f21b00627e49b99
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-22 13:50:15 +0200

    Add files via upload

---
+++ JS_opdracht09/script.js
@@ -0,0 +1,15 @@
+function add (one, two) {
+    return one+two
+}
+
+const addingUp = function (one, two) {
+    return one+two
+}
+
+const addUp = (one, two) => {
+    return one+two
+}
+
+console.log(add(1,2))
+console.log(addingUp(2,2))
+console.log(addUp(3,4))
