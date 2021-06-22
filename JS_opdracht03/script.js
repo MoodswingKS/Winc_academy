4a80c8fb6bc81f15cee9e57b8ae0bf3d40585f0d
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-22 09:54:12 +0200

    Add files via upload

---
+++ JS_opdracht03/script.js
@@ -0,0 +1,30 @@
+// Part 1
+
+const age = 28;
+
+if (age >= 18) {
+    console.log("jij mag naar binnen");
+} else {
+    console.log("jij bent te jong!");    
+}
+
+// Part 2
+
+const isFemale = true;
+
+if (!isFemale) {
+    console.log("jij bent geen vrouw");
+} else {
+    console.log("Jij bent een vrouw")
+}
+
+// Part 3
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+    console.log("Daar kun je mee thuiskomen!");
+} else {
+    console.log("Helaas, jij mag niet meer naar huis.");
+}
+
