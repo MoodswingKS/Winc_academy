7f0cb98d3371c653a83440bb3b6375e4a864e81f
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-24 16:17:35 +0200

    Add files via upload

---
+++ JS_opdracht13_guessthenumber/script.js
@@ -0,0 +1,58 @@
+let solGame = false;
+let winGame = false;
+let robotNumber = Math.floor(Math.random() * 24 + 1);
+console.log(robotNumber);
+
+const makeGuessButton = () => {
+    let btnNumber = document.createElement("btnNumber");
+    btnNumber.innerHTML = `<button id="playerNumber" onclick="playerNumber()" class="btn">RADEN MAAR</button>`;
+    document.body.appendChild(btnNumber);
+}
+
+const makeResultButton = () => {
+    let btnResult = document.createElement("result");
+    btnResult.innerHTML = `<button id="result" onclick="result()" class="btn">UITSLAG</button>`;
+    document.body.appendChild(btnResult);
+}
+
+const playerName = () => {
+    let player = prompt("Vul je naam in", "Sol");
+    if (player === 'Sol') {
+        alert("Succes " + player + "! Fair play zit bij mij hoog in het vaandel.")
+        makeGuessButton();
+        return solGame = true;
+    } else if (player != null) {
+        alert("Succes " + player + "! Ik heb al het vertrouwen in je.")
+        makeGuessButton();
+    }
+}
+
+const playerNumber = () => {
+    let inputNumber = prompt("Geef me dat nummertje G");
+    if (inputNumber != null) {
+        alert("Je kiest voor " + inputNumber + ".");
+        let guess = parseInt(inputNumber);
+        
+        if (guess == robotNumber) {
+            makeResultButton();
+            return winGame = true;
+        }
+        makeResultButton();
+    }
+}
+
+
+const result = () => {
+    if (solGame) {
+        alert("HA HA FOUT");
+        alert("GAME OVER");
+    } else if (winGame) {
+        alert("Ik moet je wat vertellen.");
+        alert("Ik loog over de medaille.");
+        alert("Dus ook al heb je gewonnen, zijn er alleen maar verliezers.");
+    } else {
+        alert("HA HA FOUT");
+        alert("Wil je het nog eens proberen?");
+        location.reload();
+    }
+}
