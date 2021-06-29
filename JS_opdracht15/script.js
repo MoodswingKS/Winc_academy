ad468f6c80b681a12deac0c2e17783749894c7b8
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-29 13:14:46 +0200

    Add files via upload

---
+++ JS_opdracht15/script.js
@@ -0,0 +1,14 @@
+const clickAlert = () => {
+    alert('button clicked!')
+}
+
+
+
+const bgChange = () => {
+    let blueBg = document.getElementsByClassName("blue-background");
+    console.log(blueBg);
+    // blueBg.classList.remove("blue-background")
+    blueBg.classList.add("red-background")
+}
+
+
