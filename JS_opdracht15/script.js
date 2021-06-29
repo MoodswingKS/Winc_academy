de3a22200d58e0142297ee5e6b8d96c36fa64db5
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-06-29 13:52:40 +0200

    Add files via upload

--- JS_opdracht15/script.js
+++ JS_opdracht15/script.js
@@ -4,11 +4,18 @@ const clickAlert = () => {
 
 
 
-const bgChange = () => {
-    let blueBg = document.getElementsByClassName("blue-background");
-    console.log(blueBg);
-    // blueBg.classList.remove("blue-background")
-    blueBg.classList.add("red-background")
-}
+// const bgChange = () => {
+//     debugger
+//     let blueBg = document.querySelector('.blue-background');
+//     console.log(blueBg);
+//     // blueBg.classList.remove("blue-background")
+//     blueBg.classList.add('.red-background');
+// }
 
+
+const bgChange = () => {
+    let blueBg = document.querySelector('.blue-background')
+    blueBg.classList.toggle('red-background');
+    console.log(blueBg)
+}
 
